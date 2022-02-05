7677109f45f475dbd89f49190d4b4ad923c06db9
AloeSwap <info@aloeswap.com>
2022-02-05 14:22:58 +0100

    asdfas

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
- var sample = await deploy('Sample');
+ var contracts = ['Sample'];
+ for (var i = 0; i < contracts.length; i++) var contract[i] = await deploy(contract[i]);
+ /*
  await collectionAdd(sample, 'Piggy');
  var ID = await sample.getCollectionsCount();
  console.log(ID.toString());
  await collectionAdd(sample, 'Duck');
  ID = await sample.getCollectionsCount();
  console.log(ID.toString());
+ */
  /*
  await propertyAdd(sample, piggy, 'Body');
  await propertyAdd(sample, piggy, 'Ears');
@@ -20,6 +23,11 @@ async function main() {
  await propertyAdd(sample, duck, 'Beak');
  await propertyAdd(sample, duck, 'Wings');
  */
+ // LOG:
+ console.log('');
+ console.log('===================================================');
+ for (var i = 0; i < contracts.length; i++) console.log('| ' + contract[i] + ': ' + contract[i].address + ' |');
+ console.log('===================================================');
 }
 
 async function deploy(name) {
